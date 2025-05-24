8d98226d0f68917b060dce6925d21b3bbecb81b0
kwakyetech <kwakyeoforiprince@gmail.com>
2025-05-24 14:43:09 +0000

    create printTeacher function with interface printTeacherFunction

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -35,3 +35,17 @@ const director1: Directors = {
 };
 
 console.log(director1);
+
+
+// Interface describing the printTeacher function
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Function implementation
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+// Example usage
+console.log(printTeacher("John", "Doe")); // Output: J. Doe
